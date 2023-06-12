0ac47259258e472286888f41ca1f356237e936e5
Darian Brown <browndarian011@gmail.com>
2023-06-11 21:02:07 -0400

    commit

---
+++ petfax/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+
+def create_app():
+    
+    app = Flask(__name__)
+
+    from . import pet
+    app.register_blueprint(pet.bp)
+
+
+    @app.route('/')
+    def index():
+        return 'this is the HOME page'
+
+    
+    return app
