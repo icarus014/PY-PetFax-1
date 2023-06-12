4db0098b2fdf484929c4206bd5369072d320c4d5
Darian Brown <browndarian011@gmail.com>
2023-06-12 01:56:35 -0400

    added styling and routes

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -2,16 +2,18 @@ from flask import Flask
 
 
 def create_app():
-    
     app = Flask(__name__)
-
-    from . import pet
-    app.register_blueprint(pet.bp)
-
+   
 
     @app.route('/')
     def index():
-        return 'this is the HOME page'
+        return 'Hello, PetFax'
+    
+    from . import pet
+    app.register_blueprint(pet.bp)
+
+    from . import fact
+    app.register_blueprint(fact.bp)
 
     
     return app
